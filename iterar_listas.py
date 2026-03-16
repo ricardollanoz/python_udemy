a934e79c3ebe49733ae29700a825d9babce75679
ricardo <ricardollanos711@gmail.com>
2026-03-16 15:06:07 -0500

    primer commit, iniciando con manejo de cadenas

---
+++ iterar_listas.py
@@ -0,0 +1,8 @@
+print("*** ITERAR LISTAS ***")
+nombres = ["Karla", "Juan", "Laura"]
+for nombre in nombres:
+    print(nombre)
+
+lista_heterogenea = [100, True, "Ricardo"]
+for prueba in lista_heterogenea:
+    print(prueba)
---
+++ manejo de cadenas/manejo_de_cadenas_.py
@@ -0,0 +1,18 @@
+correo = """
+Buenos dias
+------------
+Les aviso que la semana 1 de la historia de usuario ha sido completada
+------------
+att: Ricardo
+"""
+#print(correo)
+
+#validacion = "solo elegir 'si' o 'no'."
+#print(validacion)
+
+#mensaje = "el se llama \"1984\""
+#print(mensaje)
+
+
+
+
